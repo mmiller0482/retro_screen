1683cd0094e5bd6f0b6439b40618158998726ac8
matthewmiller <mmiller0482@gmail.com>
2025-04-19 10:16:22 -0400

    made main.py example use the local package.

--- examples/main.py
+++ examples/main.py
@@ -1,4 +1,8 @@
+import os, sys
 import pygame
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 from retro_screen import BWColorPalette
 from retro_screen import Screen
