8aa783a2a954eab913076fd242daf9c55341efc7
matthewmiller <mmiller0482@gmail.com>
2025-04-19 10:11:26 -0400

    black cleanup of __init__.py

--- retro_screen/__init__.py
+++ retro_screen/__init__.py
@@ -3,4 +3,4 @@ from .colors.default_palette import DefaultColorPalette
 from .colors.bw_palette import BWColorPalette
 from .colors.color_palette import ColorPalette
 
-__all__ = ['Screen', 'DefaultColorPalette', 'BWColorPalette', 'ColorPalette']
+__all__ = ["Screen", "DefaultColorPalette", "BWColorPalette", "ColorPalette"]
