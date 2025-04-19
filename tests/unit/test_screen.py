fa9fa8153725b5fb640058be3fe1b934ceb19047
matthewmiller <mmiller0482@gmail.com>
2025-04-19 09:54:57 -0400

    Added only thing I'm interested in testing for now.

---
+++ tests/unit/test_screen.py
@@ -0,0 +1,10 @@
+import pytest
+
+from screen import Screen
+
+
+class TestScreen:
+    def test_uninitialized_fails(self):
+        with pytest.raises(RuntimeError) as e:
+            _ = Screen()
+        assert "pygame.display must be initialized" in str(e.value)
